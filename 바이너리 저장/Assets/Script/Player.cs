24d4549ec998272a164107ffe2bde28f1d4928fa
ParkJWoo <character453@naver.com>
2017-11-19 23:44:28 +0900

    유니티 저장 방법 - 바이너리

---
+++ 바이너리 저장/Assets/Script/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int level = 1;
+    public int health = 20;
+    public int attack = 6;
+    public int defense = 4;
+
+    public void Adjust(ref int stat,int value)
+    {
+        stat += value;
+        if (stat < 1)
+        {
+            stat = 1;
+        }
+        GetComponent<PlayerDisplay>().UpdataDisplay();
+    }
+
+    public void AdjustLevel(int value)
+    {
+        Adjust(ref level, value);
+    }
+
+    public void AdjustHealth(int value)
+    {
+        Adjust(ref health, value);
+    }
+
+    public void AdjustAttack(int value)
+    {
+        Adjust(ref attack, value);
+    }
+
+    public void AdjustDefense(int value)
+    {
+        Adjust(ref defense, value);
+    }
+
+}
---
+++ 바이너리 저장/Assets/Script/SaveLoad.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SaveLoad : MonoBehaviour {
+    [Serializable]
+    public class Person //저장할 사람 정보
+    {
+        public int age;
+        public string name;
+
+        public override string ToString()
+        {
+            return "age: " + age + "name: " + name;
+        }
+    }
+
+    string filePath = string.Empty;
+    Person person;
+	// Use this for initialization
+	void Start () {
+        filePath = Application.dataPath + "/test.bin"; //저장 경로 
+        person = new Person { age = 29, name = "Jake" };
+	}
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Save")) //저장 버튼을 누를 시
+        {
+            BinarySerialize(person, filePath); //위 함수를 실행합니다
+        }
+        if (GUILayout.Button("Load")) //불러오기 버튼을 누를 시
+        { 
+            if (System.IO.File.Exists(filePath)) //파일이 존재한다면
+            {
+                person = BinaryDeserilize(filePath); //위 함수를 실행
+                Debug.Log(person);
+            }
+        }
+    }
+
+    public void BinarySerialize(Person person,string filePath) //Save 기능 
+    {
+        BinaryFormatter formatter = new BinaryFormatter(); 
+        FileStream stream = new FileStream(filePath, FileMode.Create);
+        formatter.Serialize(stream, person);
+        stream.Close();
+    }
+
+    public Person BinaryDeserilize(string filePath) //Load 기능 
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream stream = new FileStream(filePath, FileMode.Open);
+        Person person = (Person)formatter.Deserialize(stream);
+        stream.Close();
+
+        return person;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
