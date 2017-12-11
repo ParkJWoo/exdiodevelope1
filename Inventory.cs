e781be108343e92ad0d75d4061f2427746bec204
Dogy <31647710+Dogy-K@users.noreply.github.com>
2017-12-11 12:57:37 +0900

    인벤토토리에서 열쇠사용 밑바탕코드 미완
    아직쓰지말아주새오

---
+++ Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public GameObject[] inventory = new GameObject[10]; //인스펙터에 인벤토리 생성
+	
+    public void AddItem(GameObject item)
+    {
+        bool itemAdded = false;
+        //인벤토리 첫번째 오픈슬롯을 찾음
+        for(int i=0; i< inventory.Length; i++)
+        {
+            if(inventory [i] == null)
+            {
+                inventory[i] = item;
+                Debug.Log(item.name + " was added");
+                itemAdded = true;
+                break;
+            }
+        }
+
+        //인벤토리가 꽉찼을 때
+        if(!itemAdded)
+        {
+            Debug.Log("Inventory Full - Item Not Added");
+        }
+    }
+}
---
+++ PlayerInteract.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour {
+
+    // 현재 닿은 오브젝트를 표시하게 됨
+    public GameObject currentInterObj = null;
+    public InteractionObject currentInterObjScript = null;
+    public Inventory inventory;
+
+    void Update()
+    {
+        if(Input.GetButtonDown ("Interact") && currentInterObj)
+        {
+            //인벤토리에 저장되었나 보기위해 확인함
+            if(currentInterObjScript.inventory)
+            {
+                inventory.AddItem(currentInterObj);
+            }
+
+            //오브젝트랑 무언가를 함
+            currentInterObj.SendMessage("DoInteraction"); //item.Send~~로 해도 되는듯
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("InterObject"))
+        {
+            Debug.Log(other.name);
+            //커렌트인터오브젝트를 현재 닿고있는 오브젝트로 바꿈(인스펙터에 표시됨)
+            currentInterObj = other.gameObject;
+            currentInterObjScript = currentInterObj.GetComponent<InteractionObject> ();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("InterObject"))
+        {
+            //오브젝트를 벗어나면 다시 null로 바꿈
+            if(other.gameObject == currentInterObj)
+            {
+                currentInterObj = null;
+            }
+        }
+    }
+}
