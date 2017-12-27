e4a3f8f5f6c604228a28ba899f8bef22eb67c24d
kimsu802 <31647739+kimsu802@users.noreply.github.com>
2017-12-27 12:11:49 +0900

    키 입력시 창 없애고 띄우는 스크립트.
    ㅁㄴㅇㄹ

---
+++ SETACTIVE.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SETACTIVE : MonoBehaviour
+{
+  int c = 0;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {       
+            ++c;
+
+        }
+
+
+        if (c % 2 == 0)
+        {
+            GameObject.Find("esc").SetActive(false);
+        }
+ 
+
+         if (c % 2 == 1) { 
+
+
+ GameObject.Find("ugotmemother").transform.Find("esc").gameObject.SetActive(true);
+
+        }
+       
+    }
+}
