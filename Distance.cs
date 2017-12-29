d60537605d4fe3a497ed7446ed01b7153bf0e14e
kimsu802 <31647739+kimsu802@users.noreply.github.com>
2017-12-29 11:52:43 +0900

    크리쳐와 문 사이의 거리가 멀어지면 문이 위로 이동하는 스크립트
    ㅁㄴㅇㄹ

---
+++ Distance.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour {
+
+    private GameObject target = null;
+
+    public float movement = 2.0f;
+
+    public float doormovement = 1.0f;
+
+    Vector2 currentPos;
+
+    Vector2 targetPos;
+
+	// Use this for initialization
+	void Start () {
+
+        
+
+        target = GameObject.Find("무케");
+
+        currentPos = new Vector2(5.1f, -0.58f);
+
+        targetPos = new Vector2(5.1f, 4.72f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float xdis = transform.position.x - target.transform.position.x;
+
+        if (xdis > 5.0f)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, doormovement * Time.deltaTime);
+
+
+        }
+
+        else
+        {
+
+
+            transform.position = Vector2.MoveTowards(transform.position, currentPos, movement * Time.deltaTime);
+
+        }
+
+
+		
+	}
+}
