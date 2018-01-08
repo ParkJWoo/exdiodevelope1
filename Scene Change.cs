a1122aebd97fa39ce4da04a087739f460d25da36
kimsu802 <31647739+kimsu802@users.noreply.github.com>
2018-01-08 11:27:52 +0900

    Button 클릭 시 씬 전환 되고, 게임이 종료 됨.

---
+++ Scene Change.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene Change : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+
+
+
+
+		
+	}
+
+
+
+    void SceneChanger()
+    {
+
+
+
+
+        SceneManager.LoadScene(2);
+
+
+
+
+    }
+
+}
