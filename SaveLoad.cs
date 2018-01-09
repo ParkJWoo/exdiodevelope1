228d43971fbccb8c8acd6b06ba4546049f1b90b9
ParkJWoo <character453@naver.com>
2018-01-09 11:54:40 +0900

    게임 저장 코드
    ㅈㄱㄴ

---
+++ SaveLoad.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+
+public class SaveLoad : MonoBehaviour {
+
+    public void Save() //저장
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/PlayerData.dat");
+
+        PlayerData data = new PlayerData();
+
+        data.posX = transform.position.x;
+        data.posY = transform.position.y;
+
+        formatter.Serialize(file, data);
+        file.Close(); 
+    }
+
+    public void Load() //로드
+    {
+        if (File.Exists(Application.persistentDataPath + "/PlayerData.dat"))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/PlayerData.dat", FileMode.Open);
+            PlayerData data = (PlayerData)formatter.Deserialize(file);
+            file.Close();
+
+            transform.position = new Vector2(data.posX, data.posY);
+        }
+    }
+
+    [Serializable]
+    class PlayerData //플레이어 정보 클래스
+    {
+        public float posX;
+        public float posY;
+    }
+    
+}
