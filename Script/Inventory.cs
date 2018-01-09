be62e10b28b53e3da8bb97742e8e7a19535ca383
Dogy <31647710+Dogy-K@users.noreply.github.com>
2018-01-09 11:50:27 +0900

    두번째 백업
    아이템 먹으면 UI로 이동되고 사용되면 UI에서 아이템 사라지는 거 까지 업데이트 됨
    자세한 건 PlayerInteract, Inventoty 확인

--- Script/Inventory.cs
+++ Script/Inventory.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Inventory : MonoBehaviour {
 
     public GameObject[] inventory = new GameObject[10]; //인스펙터에 인벤토리 생성
-	
+    public Button[] InventoryButtons = new Button[5]; //인스펙터에 인벤토리 버튼 생성
+
     public void AddItem(GameObject item)
     {
         bool itemAdded = false;
@@ -15,6 +16,8 @@ public class Inventory : MonoBehaviour {
             if(inventory [i] == null)
             {
                 inventory[i] = item;
+                //UI 업데이트
+                InventoryButtons[i].image.overrideSprite = item.GetComponent<SpriteRenderer>().sprite;
                 Debug.Log(item.name + " was added");
                 itemAdded = true;
                 item.SendMessage("DoInteraction"); //item.Send~~로 해도 되는듯
@@ -41,5 +44,39 @@ public class Inventory : MonoBehaviour {
         }
         //아이템을 못찾았을시
         return false;
+    }
+
+    public GameObject FindItemByType(string itemType)
+    {
+        for(int i=0; i<inventory.Length; i++)
+        {
+            if(inventory[i] != null)
+            {
+                if (inventory[i].GetComponent <InteractionObject>().itemType == itemType)
+                {
+                    //우리가 보고있는 아이템을 찾음
+                    return inventory[i];
+                }
+            }
+        }
+        // 아이템 타입을 찾지못함
+        return null;
+    }
+
+    public void RemoveItem(GameObject item)
+    {
+        for (int i = 0; i < inventory.Length; i++)
+        {
+            if(inventory [i] == item)
+            {
+                //아이템을 찾음 - 그걸 삭제함
+                inventory[i] = null;
+                Debug.Log(item.name + " was removed from inventory");
+                //UI 업데이트
+                InventoryButtons[i].image.overrideSprite = null;
+
+                break;
+            }
+        }
     }
 }
--- Script/p_I_test.cs
+++ Script/p_I_test.cs
@@ -3,26 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerInteract : MonoBehaviour {
+public class p_I_test : MonoBehaviour
+{
 
     // 현재 닿은 오브젝트를 표시하게 됨
     public GameObject currentInterObj = null;
     public InteractionObject currentInterObjScript = null;
     public Inventory inventory;
 
-    void Update()
+    IEnumerator OnTriggerEnter2D(Collider2D other)
     {
         ScreenFader sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader>();
+        // "Fader" = 태그이름, 스크린페이더 스크립트 속 ScreenFader()컴포넌트를 가져옴
+
         if (Input.GetMouseButtonDown(0) && currentInterObj)
         {
             //인벤토리에 저장되었나 보기위해 확인함
-            if(currentInterObjScript.inventory)
+            if (currentInterObjScript.inventory)
             {
                 inventory.AddItem(currentInterObj);
-                
+
             }
-            
-                //오브젝트가 opened인지 체크
+
+            //오브젝트가 opened인지 체크
             if (currentInterObjScript.openable)
             {
                 //오브젝트가 locked인지 체크
@@ -35,10 +38,14 @@ public class PlayerInteract : MonoBehaviour {
                         //필요한 아이템을 찾는다
                         currentInterObjScript.locked = false;
                         Debug.Log(currentInterObj.name + " was unlocked");
-                        Destroy(currentInterObjScript);
-                        StartCoroutine(sf.FadeToBlack());
+
+                        yield return StartCoroutine(sf.FadeToBlack());
+                        // 코루틴으로 FadeToBlack 함수 실행
                         SceneManager.LoadScene("scene1");
-                        StartCoroutine(sf.FadeToClear());                  
+
+                        // 카메라가 캐릭터를 향해 이동
+                        yield return StartCoroutine(sf.FadeToClear());
+                        // 코루틴으로 FadeToClear 함수 실행
                     }
                     else
                     {
@@ -49,33 +56,16 @@ public class PlayerInteract : MonoBehaviour {
                 {
                     //오브젝트가 안잠겼을 때 - 오브젝트를 연다.
                     Debug.Log(currentInterObj.name + " is unlocked");
-             
+
                 }
             }
         }
-        //포션 사용
-        if(Input.GetButtonDown("Use Potion")) //Input키설정에서 사용한 이름 넣을것
-        {
-            //인벤토리의 포션을 체크
-            GameObject potion = inventory.FindItemByType("Health Potion");
-            if(potion != null)
-            {
-                //포션을 사용 - 이펙트적용
-                Debug.Log("I used a potion");
-                //포션을 인벤토리에서 삭제
-                inventory.RemoveItem(potion);
-            }
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
         if (other.CompareTag("InterObject"))
         {
             Debug.Log(other.name);
             //커렌트인터오브젝트를 현재 닿고있는 오브젝트로 바꿈(인스펙터에 표시됨)
             currentInterObj = other.gameObject;
-            currentInterObjScript = currentInterObj.GetComponent<InteractionObject> ();
+            currentInterObjScript = currentInterObj.GetComponent<InteractionObject>();
         }
     }
 
@@ -84,10 +74,12 @@ public class PlayerInteract : MonoBehaviour {
         if (other.CompareTag("InterObject"))
         {
             //오브젝트를 벗어나면 다시 null로 바꿈
-            if(other.gameObject == currentInterObj)
+            if (other.gameObject == currentInterObj)
             {
                 currentInterObj = null;
             }
         }
     }
+
+    
 }
