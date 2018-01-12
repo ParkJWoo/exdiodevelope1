2ed2cb1dcfec513f78909f78d0414dc210fc0e62
kimsu802 <31647739+kimsu802@users.noreply.github.com>
2018-01-12 11:14:24 +0900

    오브젝트에 따라 커서가 바뀜.
    ㄴㅇㅁㄹ

---
+++ CursorChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChange : MonoBehaviour {
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    // Use this for initialization
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+
+    }
+    private void OnMouseExit()
+    {
+        Cursor.SetCursor(null, hotSpot, cursorMode);
+
+    }
+
+}
