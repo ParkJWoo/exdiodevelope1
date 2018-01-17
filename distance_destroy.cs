1b8f57e63e61d9370ae4a5c78643dfeae0ad54f8
kimsu802 <31647739+kimsu802@users.noreply.github.com>
2018-01-17 09:17:18 +0900

    아이템 근처에서 클릭했을때만 아이템 얻어지는것.
    아이템 얻는 매커니즘을 잘 몰라서 일단 파괴하는것으로 대체함. 
    
    아이템 마다 이 스크립트를 첨부하면 될듯.

---
+++ distance_destroy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class distance_destroy : MonoBehaviour {
+
+    private GameObject target = null;
+
+    
+
+	// Use this for initialization
+	void Start () {
+
+
+        target = GameObject.Find("무케");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+       		
+	}
+
+    private void OnMouseDown()
+    {
+
+        float xdist = transform.position.x - target.transform.position.x;
+
+        if (xdist < 2.0f) 
+        {
+            Destroy(GameObject.Find("사슬"));
+
+        }
+    }
+}
