353f5ddad0bd35dca15de80754c891764cebc5c4
KimJeeHyung <31695396+KimJeeHyung@users.noreply.github.com>
2018-01-19 12:59:57 +0900

    따라가기(크리쳐)

---
+++ follow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow : MonoBehaviour {
+    public float xDis = 0;
+    public float speed = 1.0f;
+    private GameObject target = null;
+    private Vector2 follower;
+    private Animator animator;
+    private bool isFacingRight = true;
+    new SpriteRenderer renderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        follower.y = transform.position.y;
+        animator = this.GetComponent<Animator>();
+        renderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        float dis = Vector2.Distance(transform.position, target.transform.position);
+         if (transform.position.x < 6.0f)
+        {
+
+            if (dis < xDis)
+            {
+                Flip();
+                animator.SetBool("isMoving", true);
+                transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                Flip();
+                animator.SetBool("isMoving", false);
+            }
+        }
+        else if(transform.position.x >= 6)
+        {
+            animator.SetBool("isMoving", false);
+        }
+	}
+    
+    void Flip()
+    {
+        if(target.transform.position.x < this.transform.position.x && isFacingRight == true)
+        {
+            isFacingRight = !isFacingRight;
+            renderer.flipX = true;
+        }
+        else if(target.transform.position.x > this.transform.position.x && isFacingRight == false)
+        {
+            isFacingRight = !isFacingRight;
+            renderer.flipX = false;
+        }
+    }
+}
