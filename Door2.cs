946f406348be07e9aa59f87a9b548f71dd267a1d
KimJeeHyung <31695396+KimJeeHyung@users.noreply.github.com>
2018-01-26 14:42:21 +0900

    큰방 왼쪽 문
    주인공이 이 문으로 나옴과 동시에 충돌
    그 후 사슬문과 남주 오브젝트 삭제(destroy)

---
+++ Door2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door2 : MonoBehaviour {
+
+    private GameObject door1, follower;
+
+	// Use this for initialization
+	void Start () {
+        door1 = GameObject.Find("문");
+        follower = GameObject.Find("남주");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("door1 open");
+            door1.GetComponent<Door>().transform.position = new Vector2(43f, 0.5f);
+            Destroy(follower);
+            Destroy(door1);
+        }
+    }
+}
