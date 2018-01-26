c0bae8b59e48cd33de3cefe8c22259459876df16
KimJeeHyung <31695396+KimJeeHyung@users.noreply.github.com>
2018-01-26 14:39:35 +0900

    문 수정본(재)
    복도 오른쪽 끝 사슬문

--- Door.cs
+++ Door.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -26,23 +26,29 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target.transform.position.x <= 24f)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, targetPos, doormovement * Time.deltaTime);
-        }
-        else
-        {
-            transform.position = Vector2.MoveTowards(transform.position, currentPos, doormovement * Time.deltaTime);
-        }
+        //if (target.transform.position.x <= 24f)
+        //{
+        //    transform.position = Vector2.MoveTowards(transform.position, targetPos, doormovement * Time.deltaTime);
+        //}
+        //else
+        //{
+        //    transform.position = Vector2.MoveTowards(transform.position, currentPos, doormovement * Time.deltaTime);
+        //}
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         //주인공이 문과 충돌하면
         if (collision.tag == "Player")
         {
             Debug.Log("We arrived at the door");
-            follower.GetComponent<Follow>().enabled = false;    //남주가 움직이지 않는다
+            if(Input.GetMouseButtonDown(0))
+            {
+                follower.GetComponent<Follow>().enabled = false;    //남주가 움직이지 않는다
+            }
+            
         }
     }
+
+   
 }
