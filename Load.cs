5b90f796cbc969ef8ea796cd7b0aa0bd289a02c2
ParkJWoo <character453@naver.com>
2018-01-31 23:23:24 +0900

    로드 부분 수정 필요함
    ㅈㄱㄴ

--- Load.cs
+++ Load.cs
@@ -4,46 +4,60 @@ using UnityEngine;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 using System;
+using UnityEngine.SceneManagement;
 
 public class Load : MonoBehaviour {
 
-    Save savedData;
-    PlayerData data;
-    
-    private void Start()
-    {
-        savedData = GameObject.FindGameObjectWithTag("player").GetComponent<Save>();
-    }
+	Save savedData;
+	PlayerData data;
+	public bool isLoaded; 
 
-    public void Loader()
-    {
-        if (File.Exists(Application.persistentDataPath + "/playerData.dat"))
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/playerData.dat", FileMode.Open);
-            PlayerData data = (PlayerData)bf.Deserialize(file);
-            
-            transform.position
-            
-           
-            Debug.Log("Loading");
+	private void Start()
+	{
+		savedData = GameObject.FindGameObjectWithTag("player").GetComponent<Save>();
+		isLoaded = false;
+	}
 
-            //SceneManager.LoadSceneAsync("large_room", LoadSceneMode.Single);
+	public void Loader()
+	{
+		if (File.Exists(Application.persistentDataPath + "/playerData.dat"))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/playerData.dat", FileMode.Open);
+			PlayerData data = (PlayerData)bf.Deserialize(file);
 
-            //  SceneManager.LoadScene("large_room");
-        }
+			//transform.position = new Vector2 (data.playerPosX_L, data.playerPosY_L);
 
-        else
-        {
-            Debug.Log("파일이 안읽혀요 ㅠㅠ");
-        }
 
-    }
+			Debug.Log("Loading");
 
-    [Serializable]
-    class PlayerData
-    {
-        public float playerPosX_L;
-        public float playerPosY_L;
-    }
+			//SceneManager.LoadSceneAsync("large_room", LoadSceneMode.Single);
+
+			//  SceneManager.LoadScene("large_room");
+		}
+
+		else
+		{
+			Debug.Log("파일이 안읽혀요 ㅠㅠ");
+		}
+
+	}
+
+	public void callScene()
+	{
+		isLoaded = true;
+		Loader ();
+		Application.LoadLevel (3);
+
+	}
+
+	//씬을 불러오는 코드
+
+	[Serializable]
+	class PlayerData
+	{
+		public float playerPosX_L;
+		public float playerPosY_L;
+		//public int playerPosScene;
+	}
 }
--- Save.cs
+++ Save.cs
@@ -7,30 +7,30 @@ using System;
 
 public class Save : MonoBehaviour {
 
-    public void Saver()
-    {
-        //바이너리 파일 생성
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/playerData.dat");
+	public void Saver()
+	{
+		//바이너리 파일 생성
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/playerData.dat");
 
-        //저장하고자 하는 오브젝트의 정보
-        PlayerData data = new PlayerData();
+		//저장하고자 하는 오브젝트의 정보
+		PlayerData data = new PlayerData();
 
-        data.playerPosX = transform.position.x;
-        data.playerPosY = transform.position.y;
+		data.playerPosX = transform.position.x;
+		data.playerPosY = transform.position.y;
+		//data.playerPosScene= //현재 씬 번호 불러오는 거
+		bf.Serialize(file, data);
+		file.Close();
 
-        bf.Serialize(file, data);
-        file.Close();
+		Debug.Log("saving..");
 
-        Debug.Log("saving..");
+	}
 
-    }
-
-    [Serializable]
-    class PlayerData
-    {
-        public float playerPosX;
-        public float playerPosY;
-    }
+	[Serializable]
+	class PlayerData
+	{
+		public float playerPosX;
+		public float playerPosY;
+		//public int playerPosScene;
+	}
 }
-
