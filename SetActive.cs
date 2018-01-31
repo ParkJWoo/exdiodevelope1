e51b6a761f285bac3dba20a679854a9fa51ad8d3
Dogy <31647710+Dogy-K@users.noreply.github.com>
2018-01-31 13:24:07 +0900

    배겁
    다른씬에서 아이템 먹었을 시 다른씬으로 넘어갔을 때 사용 안되는 다량 버그 수정
    수정버그
    그버정수
    정버수그

---
+++ SetActive.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActive : MonoBehaviour {
+    private GameObject mirror_pass;
+    private GameObject strong_box;
+    private GameObject pswd;
+
+    void Start () {
+        mirror_pass = GameObject.Find("mirror_password");
+        mirror_pass.SetActive(false);
+
+        strong_box = GameObject.Find("strong_box");
+        strong_box.SetActive(false);
+    }
+	
+}
