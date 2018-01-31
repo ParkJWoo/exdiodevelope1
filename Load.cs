797685b8f6d4052eba3b4dd7119ee5d111cc93d3
ParkJWoo <character453@naver.com>
2018-01-31 14:37:07 +0900

    save load 리로이
    ㅁ

---
+++ Load.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System;
+
+public class Load : MonoBehaviour {
+
+    Save savedData;
+    PlayerData data;
+    
+    private void Start()
+    {
+        savedData = GameObject.FindGameObjectWithTag("player").GetComponent<Save>();
+    }
+
+    public void Loader()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerData.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerData.dat", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            
+            transform.position
+            
+           
+            Debug.Log("Loading");
+
+            //SceneManager.LoadSceneAsync("large_room", LoadSceneMode.Single);
+
+            //  SceneManager.LoadScene("large_room");
+        }
+
+        else
+        {
+            Debug.Log("파일이 안읽혀요 ㅠㅠ");
+        }
+
+    }
+
+    [Serializable]
+    class PlayerData
+    {
+        public float playerPosX_L;
+        public float playerPosY_L;
+    }
+}
---
+++ Save.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+
+public class Save : MonoBehaviour {
+
+    public void Saver()
+    {
+        //바이너리 파일 생성
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerData.dat");
+
+        //저장하고자 하는 오브젝트의 정보
+        PlayerData data = new PlayerData();
+
+        data.playerPosX = transform.position.x;
+        data.playerPosY = transform.position.y;
+
+        bf.Serialize(file, data);
+        file.Close();
+
+        Debug.Log("saving..");
+
+    }
+
+    [Serializable]
+    class PlayerData
+    {
+        public float playerPosX;
+        public float playerPosY;
+    }
+}
+
