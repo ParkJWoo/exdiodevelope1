ef169b83359fd89e23b608bb7b4702a2e2425edd
ParkJWoo <character453@naver.com>
2018-02-01 23:05:45 +0900

    Add files via upload

--- Load.cs
+++ Load.cs
@@ -9,64 +9,60 @@ using UnityEngine.SceneManagement;
 public class Load : MonoBehaviour
 {
 
-    Save savedData;
-    PlayerData data;
-    public bool isLoaded;
-    public PlayerControl pc;
+	Save savedData;
+	PlayerData data;
+	public bool isLoaded;
+	public GameObject playerRef;
 
-    private void Start()
-    {
-        savedData = GameObject.FindGameObjectWithTag("player").GetComponent<Save>();
-        isLoaded = false;
-        pc = GameObject.FindGameObjectWithTag("player").GetComponent<PlayerControl>(); //playercontrol 코드 참조
-    }
+	private void Start()
+	{
+		savedData = GameObject.FindGameObjectWithTag("Player").GetComponent<Save>();
+		isLoaded = false;
+		//pc = GameObject.FindGameObjectWithTag("player").GetComponent<PlayerControl>(); //playercontrol 코드 참조
+	}
 
-    public void Loader()
-    {
-        PlayerControl playerInstance;
+	public void Loader()
+	{
+		//PlayerControl playerInstance;
 
-        if (File.Exists(Application.persistentDataPath + "/playerData.dat"))
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/playerData.dat", FileMode.Open);
-            PlayerData data = (PlayerData)bf.Deserialize(file);
+		if (File.Exists(Application.persistentDataPath + "/playerData.dat"))
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Application.persistentDataPath + "/playerData.dat", FileMode.Open);
+			PlayerData data = (PlayerData)bf.Deserialize(file);
 
-            //transform.position = new Vector2 (data.playerPosX_L, data.playerPosY_L);
+			//transform.position = new Vector2 (data.playerPosX_L, data.playerPosY_L);
 
 
-            Debug.Log("Loading");
+			Debug.Log("Loading");
 
-            //외부스크립트 상호작용할 코드 넣을 자리
-            playerInstance = Instantiate(pc) as PlayerControl; //pc변수 인스턴스화 
-            playerInstance.GetComponent<PlayerControl>().transform = data.transform.position;
-            //pc.GetComponent<PlayerControl>().transform = data.transform;
-            //SceneManager.LoadSceneAsync("large_room", LoadSceneMode.Single);
+			//외부스크립트 상호작용할 코드 넣을 자리
+			//playerInstance = Instantiate(pc) as PlayerControl; //pc변수 인스턴스화 
+			//playerInstance.GetComponent<PlayerControl>().transform = data.transform.position;
+			//pc.GetComponent<PlayerControl>().transform = data.transform;
+			//SceneManager.LoadSceneAsync("large_room", LoadSceneMode.Single);
 
-            //  SceneManager.LoadScene("large_room");
-        }
+			//  SceneManager.LoadScene("large_room");
+		}
+	}
 
-        else
-        {
-            Debug.Log("파일이 안읽혀요 ㅠㅠ");
-        }
+	public void callScene()
+	{
+		Debug.Log ("Lodinging");
+		isLoaded = true;
+		Loader();
+		Application.LoadLevel(3);
+		playerRef = GameObject.Find ("GameObject");
 
-    }
+	}
 
-    public void callScene()
-    {
-        isLoaded = true;
-        Loader();
-        Application.LoadLevel(3);
+	//씬을 불러오는 코드
 
-    }
-
-    //씬을 불러오는 코드
-
-    [Serializable]
-    class PlayerData
-    {
-        public float playerPosX_L;
-        public float playerPosY_L;
-        //public int playerPosScene;
-    }
+	[Serializable]
+	class PlayerData
+	{
+		public float playerPosX_L;
+		public float playerPosY_L;
+		//public int playerPosScene;
+	}
 }
--- Save.cs
+++ Save.cs
@@ -8,30 +8,35 @@ using System;
 public class Save : MonoBehaviour
 {
 
-    public void Saver()
-    {
-        //바이너리 파일 생성
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Application.persistentDataPath + "/playerData.dat");
+	void OnMouseDown()
+	{
+		Saver ();
+	}
 
-        //저장하고자 하는 오브젝트의 정보
-        PlayerData data = new PlayerData();
+	public void Saver()
+	{
+		//바이너리 파일 생성
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/playerData.dat");
 
-        data.playerPosX = transform.position.x;
-        data.playerPosY = transform.position.y;
-        //data.playerPosScene= //현재 씬 번호 불러오는 거
-        bf.Serialize(file, data);
-        file.Close();
+		//저장하고자 하는 오브젝트의 정보
+		PlayerData data = new PlayerData();
 
-        Debug.Log("saving..");
+		data.playerPosX = transform.position.x;
+		data.playerPosY = transform.position.y;
+		//data.playerPosScene= //현재 씬 번호 불러오는 거
+		bf.Serialize(file, data);
+		file.Close();
 
-    }
+		Debug.Log("saving..");
 
-    [Serializable]
-    class PlayerData
-    {
-        public float playerPosX;
-        public float playerPosY;
-        //public int playerPosScene;
-    }
+	}
+
+	[Serializable]
+	class PlayerData
+	{
+		public float playerPosX;
+		public float playerPosY;
+		//public int playerPosScene;
+	}
 }
