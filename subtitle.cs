4968f7e333cff2264f566e8c48c2faa5e9e46453
kimsu802 <31647739+kimsu802@users.noreply.github.com>
2018-02-01 12:49:46 +0900

    자막 한글자한글자씩 출력 (수정본)
    ㄴㅁㄹ

--- subtitle.cs
+++ subtitle.cs
@@ -2,30 +2,86 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class subtitle : MonoBehaviour {
+public class subtitle : MonoBehaviour
+{
+
 
     public GUIStyle TextStyle;
-   
-    Font myFont = (Font)Resources.Load("fonts/NANUMPEN", typeof(Font)); // 네이버 나눔글꼴
+
+    public string[] SUBTITLE;
+
+    private int i = 0;
+
+    private const float WAIT_TIME = 0.1f;
+
+    private float waitTimer = 0.0f;
+
+    
+
+    private string typewriterText = "";
+
+    private int currentIndex = 0;
+
 
 
     // Use this for initialization
     void Start()
     {
 
-        TextStyle.font = myFont;
         TextStyle.normal.textColor = Color.white;
         TextStyle.fontSize = 20;
+
+
     }
 
     // Update is called once per frame
     void Update()
+
     {
- 
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            i++;
+            currentIndex = 0;
+            typewriterText = "";
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            i--;
+            currentIndex = 0;
+            typewriterText = "";
+
+        }
+
+
+        waitTimer += Time.deltaTime;
+
+        if (waitTimer > WAIT_TIME && currentIndex < SUBTITLE[i].Length)
+        {
+            typewriterText += SUBTITLE[i][currentIndex];
+            waitTimer = 0.0f;
+            ++currentIndex;
+        }
     }
 
-    void OnGUI()
-    {   
-        GUI.Label(new Rect(0.0f, 0.0f, 400.0f, 200.0f), "폭파 시간까지 얼마 남지 않았다.", TextStyle);
+
+
+void OnGUI()
+    {
+
+        GUI.Label(new Rect(Screen.width / 8, Screen.height / 3, 400.0f, 200.0f), typewriterText);
     }
+
+
 }
+
+    
+
+
+       
+    
+
+       
+
+    
